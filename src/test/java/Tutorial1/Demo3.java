3c509bc116f3ac00b9fa34537fce326659ac0b76
Pramod077 <71170244+Pramod077@users.noreply.github.com>
2024-07-15 23:06:29 +0530

    Search Test cases written by Amit Automation Tester

---
+++ src/test/java/Tutorial1/Demo3.java
@@ -0,0 +1,9 @@
+package Tutorial1;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		System.out.println("search user in PIM page test cases written by Amit Automation Tester");
+	}
+
+}
